da7c2695a18358c0845c15deb1ce2e72313a4f34
fahad-akbar <fahad.akbar@devisinc.com>
2022-09-06 22:54:14 +0500

    * Disabled Mark Complete Button, Error on past date on bug creation

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -4,15 +4,23 @@ class Bug < ApplicationRecord
   validates :title, :bugtype, presence: true
   validate :attached_screenshot_type
   validates :title, uniqueness: { case_sensitive: false }
+  validate :not_past_date
 
   has_one_attached :screenshot
   belongs_to :project
   belongs_to :user
 
-  enum bugtype: { bug: 0, feature: 1 }
-  enum status: { newbug: 0, started: 1, resolved: 2, completed: 3 }
+  enum bugtype: { Bug: 0, Feature: 1 }
+  enum status: { Newbug: 0, Started: 1, Resolved: 2, Completed: 3 }
 
   private
+
+def not_past_date
+  if self.deadline < Date.today
+    errors.add(:deadline, ' of the bug can not be in the past')
+  end
+end
+
 
   def attached_screenshot_type
     errors.add(:screenshot, 'must be PNG or GIF file') if screenshot.attached? && !screenshot.content_type.in?(%w[image/png image/gif])
