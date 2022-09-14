eb67ad45bb79d28e4650dbff694d446a8612a74e
fahad-akbar <fahad.akbar@devisinc.com>
2022-09-14 23:57:14 +0500

    * Made project controller slim

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,10 @@
 
 class ProjectsController < ApplicationController
   before_action :authenticate_user!, except: [:welcome]
-  before_action :fetch_project, only: %i[show edit]
   before_action :fetch_user, only: %i[create destroy update]
+   before_action :fetch_create_project, only: %i[create]
+  before_action :fetch_project, only: %i[show edit update]
+  before_action :fetch_destroy_project, only: %i[destroy]
 
   def welcome; end
 
@@ -12,7 +14,6 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    fetch_project
     @developers = User.with_role :Developer
     @qas = User.with_role :QA
   end
@@ -25,35 +26,38 @@ class ProjectsController < ApplicationController
   def edit; end
 
   def create
-    fetch_user
-    @project = @user.projects.new(project_params)
     authorize @project
     if @project.save
-      redirect_to @project
+      redirect_to @project, notice: ' Project has been created successfully '
     else
       redirect_to new_project_url, notice: ' Project was not created '
     end
   end
 
   def update
-    fetch_project
     if @project.update(project_params)
-      redirect_to @project
+      redirect_to @project, notice: ' Project has been updated successfully '
     else
       redirect_to edit_project_url, notice: ' Project was not updated '
     end
   end
 
   def destroy
-    fetch_project
     if @project.destroy
-      redirect_to projects_path
+      redirect_to projects_path, notice: ' Project has been destroyed successfully '
     else
       redirect_to project_path, notice: ' Project was not deleted '
     end
   end
 
-  private
+private
+  def fetch_create_project
+    @project = @user.projects.new(project_params)
+  end
+
+  def fetch_destroy_project
+    @project = @user.projects.find(params[:id])
+  end
 
   def project_params
     params.require(:project).permit(:title, :description)
