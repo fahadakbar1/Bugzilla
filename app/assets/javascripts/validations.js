c4e8f7f478bd81c2a5995bfae7f4b0113619c00f
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2022-10-04 22:50:25 +0500

    Last Evaluation Points * Added partial for form of new and edit bug view * Frontend validation for select fields * Display developers and qas in good form * removed before_action from all controller and added to only application controller * Refactored Prouse controller

--- app/assets/javascripts/validations.js
+++ app/assets/javascripts/validations.js
@@ -17,9 +17,33 @@ $('form').validate({
      },
       'user[username]': 'required',
       'user[email]': 'required',
-      'user[password]': 'required',
+      'user[password]' : {
+       required: true,
+       minlength: 6
+     },
       'user[password_confirmation]': 'required',
      }
 });
+
+$('.dev_form').submit(function(event){
+
+  var $form = $(this);
+
+  var x = $form.find("select");
+  if (x.val() == null){
+    alert("Developer was not selected");
+    return false;}
+});
+
+$('.qa_form').submit(function(event){
+
+  var $form = $(this);
+
+  var x = $form.find("select");
+  if (x.val() == null){
+    alert("QA was not selected");
+    return false;}
+});
+
 });
 });
--- app/controllers/prouse_controller.rb
+++ app/controllers/prouse_controller.rb
@@ -7,75 +7,53 @@ class ProuseController < ApplicationController
   before_action :select_qa, only: %i[add_qa remove_qa]
 
   def add_developer
-
     respond_to do |format|
-    if @developer.nil?
-      format.html { redirect_to project_path(@projid), notice: 'Please select any developer to be added' }
-      format.json { render json: @project.errors, status: :unprocessable_entity }
-
-    elsif Prouse.exists?(Prouse.check_existing_developer(params[:project], @developer.id))
-      format.html { redirect_to project_path(@projid), notice: "#{@dev} is already the developer of this project" }
-      format.json { render :show, status: :ok, location: @project }
-    else
-      Prouse.create(project_id: params[:project], user_id: @developer.id)
-      format.html { redirect_to project_path(@projid), notice: "Developer #{@dev} has been added to this project" }
+      if Prouse.exists?(Prouse.check_existing_developer(params[:project], @developer.id))
+        format.html { redirect_to project_path(@projid), notice: "#{@dev} is already the developer of this project" }
+      else
+        Prouse.create(project_id: params[:project], user_id: @developer.id)
+        format.html { redirect_to project_path(@projid), notice: "Developer #{@dev} has been added to this project" }
+      end
       format.json { render :show, status: :ok, location: @project }
     end
   end
-end
 
   def add_qa
     respond_to do |format|
-    if @selected_qa.nil?
-      format.html { redirect_to project_path(@projid), notice: 'Please select any QA to be added' }
-      format.json { render json: @project.errors, status: :unprocessable_entity }
-
-    elsif Prouse.exists?(Prouse.check_existing_developer(params[:project], @selected_qa.id))
-      format.html { redirect_to project_path(@projid), notice: "#{@qa} is already the QA of this project" }
-      format.json { render :show, status: :ok, location: @project }
-    else
-      Prouse.create(project_id: params[:project], user_id: @selected_qa.id)
-      format.html { redirect_to project_path(@projid), notice: "QA #{@qa} has been added to this project" }
+      if Prouse.exists?(Prouse.check_existing_developer(params[:project], @selected_qa.id))
+        format.html { redirect_to project_path(@projid), notice: "#{@qa} is already the QA of this project" }
+      else
+        Prouse.create(project_id: params[:project], user_id: @selected_qa.id)
+        format.html { redirect_to project_path(@projid), notice: "QA #{@qa} has been added to this project" }
+      end
       format.json { render :show, status: :ok, location: @project }
     end
   end
-end
 
   def remove_developer
     respond_to do |format|
-    if @developer.nil?
-      format.html { redirect_to project_path(@projid), notice: 'Please select any developer to be removed' }
-      format.json { render json: @project.errors, status: :unprocessable_entity }
+      if Prouse.exists?(Prouse.check_existing_developer(params[:project], @developer.id))
+        Prouse.delete(Prouse.check_existing_developer(params[:project], @developer.id))
+        format.html { redirect_to project_path(@projid), notice: "Developer #{@dev} has been removed from this project" }
 
-    elsif Prouse.exists?(Prouse.check_existing_developer(params[:project], @developer.id))
-      Prouse.delete(Prouse.check_existing_developer(params[:project], @developer.id))
-      format.html {redirect_to project_path(@projid), notice: "Developer #{@dev} has been removed from this project"}
+      else
+        format.html { redirect_to project_path(@projid), notice: "#{@dev} is not the developer of this project" }
+
+      end
       format.json { render :show, status: :ok, location: @project }
-
-    else
-      format.html { redirect_to project_path(@projid), notice: "#{@dev} is not the developer of this project" }
-      format.json { render :show, status: :ok, location: @project }
-
     end
   end
-end
 
   def remove_qa
     respond_to do |format|
-    if @selected_qa.nil?
-      format.html { redirect_to project_path(@projid), notice: 'Please select any QA to be removed' }
-      format.json { render json: @project.errors, status: :unprocessable_entity }
+      if Prouse.exists?(Prouse.check_existing_developer(params[:project], @selected_qa.id))
+        Prouse.delete(Prouse.check_existing_developer(params[:project], @selected_qa.id))
+        format.html { redirect_to project_path(@projid), notice: "QA #{@qa} has been removed from this project" }
 
-    elsif Prouse.exists?(Prouse.check_existing_developer(params[:project], @selected_qa.id))
-      Prouse.delete(Prouse.check_existing_developer(params[:project], @selected_qa.id))
-      format.html { redirect_to project_path(@projid), notice: "QA #{@qa} has been removed from this project" }
-      format.json { render :show, status: :ok, location: @project }
-
-    else
-      redirect_to project_path(@projid), notice: "#{@qa} is not the QA of this project"
+      else
+        redirect_to project_path(@projid), notice: "#{@qa} is not the QA of this project"
+      end
       format.json { render :show, status: :ok, location: @project }
     end
   end
 end
-end
-
